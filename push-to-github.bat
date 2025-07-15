08fa04f1f8c260587f54e47e58c758dc86cfa47c
Patryk Szymański <Patryk.szymanski27@onet.pl>
2025-07-15 20:15:12 +0200

    dodanie zdjec

---
+++ push-to-github.bat
@@ -0,0 +1,21 @@
+@echo off
+echo ================================
+echo  🔁 Git: commit & push start
+echo ================================
+
+cd /d %~dp0
+
+REM Dodanie zmian
+git add .
+
+REM Commit z domyślnym komunikatem (możesz zmienić poniżej)
+set /p MSG=📝 Podaj opis zmian: 
+git commit -m "%MSG%"
+
+
+REM Wysyłanie zmian
+git push origin main
+
+echo ================================
+echo ✅ Wysłano na GitHuba!
+pause

c044cc53ab5f881cc7f2abc38a9f74f1bef197c3
Patryk Szymański <Patryk.szymanski27@onet.pl>
2025-07-25 19:53:17 +0200

    update

--- push-to-github.bat
+++ push-to-github.bat
@@ -1,22 +1,24 @@
 @echo off
-echo ================================
-echo Git: commit & push start
-echo ================================
+echo ===============================
+echo Git: commit, pull --rebase & push
+echo ===============================
 
 cd /d %~dp0
 
 REM Dodanie zmian
 git add .
 
-REM Commit z domyślnym komunikatem (możesz zmienić poniżej)
+REM Commit z komunikatem użytkownika
 set /p MSG=Podaj opis zmian: 
 git commit -m "%MSG%"
 
+REM Pobranie zmian ze zdalnego repozytorium
+git pull --rebase origin main
 
-REM Wysyłanie zmian
+REM Wysłanie zmian
 git push origin main
 
-echo ================================
-echo Wyslano na GitHuba!
-echo ================================
+echo ===============================
+echo Wysłano na GitHuba!
+echo ===============================
 pause
